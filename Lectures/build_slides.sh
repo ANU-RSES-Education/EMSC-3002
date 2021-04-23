c259ac279237d27fa0684f02bc9b1010887356bf
Louis Moresi <louis.moresi@anu.edu.au>
2021-04-23 21:59:41 +1000

    Adding slides via reveal-md (#5)
    * Update ServerID.md
    
    * Strip back examples, add lecture notes

---
+++ Lectures/build_slides.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+# This will build all the reveal.md files it finds in the root directory
+# These files are also ignored by the jupyterbook script
+
+
+if ! command -v reveal-md &> /dev/null
+then
+    npm install -g reveal-md 
+fi
+
+
+mkdir -p static_slides
+reveal-md  --static static_slides/slideshows --theme white --glob '**/*.reveal.md' --static-dirs images
