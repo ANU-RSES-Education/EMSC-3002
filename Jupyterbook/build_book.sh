2249f8c7320c3e3184c7be43f7073c79fd2b22a7
Louis Moresi <louis.moresi@anu.edu.au>
2021-07-12 16:35:42 +1000

    Minor updates for global tectonics stuff

--- Jupyterbook/build_book.sh
+++ Jupyterbook/build_book.sh
@@ -6,4 +6,13 @@ cd Lectures
 source build_slides.sh 
 cd ..
 
-~/.local/bin/jupyter-book build . 
+jupyter-book build . 
+
+
+# This is best done by hand so it updates the slides even 
+# if there is no work to be done in rebuilding the book 
+
+cp -r Lectures/static_slides/slideshows _build/html
+
+mkdir -p _build/html/Figures/Movies
+cp -r Figures/Movies _build/html/Figures
