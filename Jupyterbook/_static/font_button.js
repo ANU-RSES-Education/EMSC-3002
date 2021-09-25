2f43444bae0b337072390a210c924fcf6ecdef81
Louis Moresi <louis.moresi@anu.edu.au>
2021-09-25 13:34:24 +1000

    fix up js and commit

--- Jupyterbook/_static/font_button.js
+++ Jupyterbook/_static/font_button.js
@@ -1,3 +1,5 @@
+// Accessibility changes (these are a bit hacky / repetitive while I debug javascript)
+
 
 // Persistent variable - defaults to pretty font
 
@@ -5,20 +7,60 @@ if (localStorage.getItem('legibility') != 'legible') {
     localStorage.setItem('legibility', 'pretty');
 }
 
-// Action this on page load, will use persistent value
+// Persistent variable - scale defaults to 1.0
+
+if (localStorage.getItem('fontScale') == null) {
+    localStorage.setItem('fontScale', 1.0);
+}
+
+// Action this on page load, will use persistent values
+
+// Font scale factor
+
+fontScalingFactor = localStorage.getItem('fontScale')
+document.documentElement.style.setProperty('--pst-font-scale-factor', fontScalingFactor);
+
+// Font families
 
 savedStyle = localStorage.getItem('legibility')
-if (savedStyle == 'legible') {
-    legibleFontFamily = getComputedStyle(document.documentElement).getPropertyValue('--pst-font-family-legible');
-    legibleFontFamilyH = getComputedStyle(document.documentElement).getPropertyValue('--pst-font-family-legible-headers');
-    document.documentElement.style.setProperty('--pst-font-family-base', legibleFontFamily);
-    document.documentElement.style.setProperty('--pst-font-family-heading', legibleFontFamilyH);
+if (savedStyle == 'legible') 
+    legibleFontSetter('legible')
+else 
+    legibleFontSetter('pretty')
+
+
+// Change font size
+
+function fontScaler(scale){
+    if(scale == 0.0){
+        if (localStorage.getItem('legibility') == 'legible')
+            fontScalingFactor = 1.25;
+        else
+            fontScalingFactor = 1.0;
+    }
+    else {
+        fontScalingFactor = localStorage.getItem('fontScale')
+        fontScalingFactor *= scale
+    }
+    localStorage.setItem('fontScale', fontScalingFactor);
+    document.documentElement.style.setProperty('--pst-font-scale-factor', fontScalingFactor);
 }
-else {
-    prettyFontFamily = getComputedStyle(document.documentElement).getPropertyValue('--pst-font-family-pretty');
-    prettyFontFamilyH = getComputedStyle(document.documentElement).getPropertyValue('--pst-font-family-pretty-headers');
-    document.documentElement.style.setProperty('--pst-font-family-base', prettyFontFamily);
-    document.documentElement.style.setProperty('--pst-font-family-heading', prettyFontFamilyH);
+
+// Change the font family 
+
+function legibleFontSetter(fontType){
+    if (fontType == 'legible'){
+        legibleFontFamily = getComputedStyle(document.documentElement).getPropertyValue('--pst-font-family-legible');
+        legibleFontFamilyH = getComputedStyle(document.documentElement).getPropertyValue('--pst-font-family-legible-headers');
+        document.documentElement.style.setProperty('--pst-font-family-base', legibleFontFamily);
+        document.documentElement.style.setProperty('--pst-font-family-heading', legibleFontFamilyH);
+    }
+    else {
+        prettyFontFamily = getComputedStyle(document.documentElement).getPropertyValue('--pst-font-family-pretty');
+        prettyFontFamilyH = getComputedStyle(document.documentElement).getPropertyValue('--pst-font-family-pretty-headers');
+        document.documentElement.style.setProperty('--pst-font-family-base', prettyFontFamily);
+        document.documentElement.style.setProperty('--pst-font-family-heading', prettyFontFamilyH);
+    }
 
 }
 
@@ -28,17 +70,13 @@ function legibleFontSwitcher() {
     savedStyle = localStorage.getItem('legibility')
     if (savedStyle == 'pretty') {
         thisStyle = 'legible'
-        legibleFontFamily = getComputedStyle(document.documentElement).getPropertyValue('--pst-font-family-legible');
-        legibleFontFamilyH = getComputedStyle(document.documentElement).getPropertyValue('--pst-font-family-legible-headers');
-        document.documentElement.style.setProperty('--pst-font-family-base', legibleFontFamily);
-        document.documentElement.style.setProperty('--pst-font-family-heading', legibleFontFamilyH);
+        fontScaler(1.25);
+        legibleFontSetter('legible')
     }
     else {
         thisStyle = 'pretty'; 
-        prettyFontFamily = getComputedStyle(document.documentElement).getPropertyValue('--pst-font-family-pretty');
-        prettyFontFamilyH = getComputedStyle(document.documentElement).getPropertyValue('--pst-font-family-pretty-headers');
-        document.documentElement.style.setProperty('--pst-font-family-base', prettyFontFamily);
-        document.documentElement.style.setProperty('--pst-font-family-heading', prettyFontFamilyH);
+        fontScaler(0.8);
+        legibleFontSetter('pretty')
     }
     localStorage.setItem('legibility', thisStyle);
 }
